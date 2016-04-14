07c5f7f6320a2436493b8fdd173f6e1cfb6d0dc6
charlesmadere <charlesrmadere@gmail.com>
2016-04-13 23:02:47 -0700

    moved PalettePostprocessor into PaletteUtils class

--- project/app/src/main/java/com/charlesmadere/hummingbird/misc/PaletteUtils.java
+++ project/app/src/main/java/com/charlesmadere/hummingbird/misc/PaletteUtils.java
@@ -1,16 +1,25 @@
 package com.charlesmadere.hummingbird.misc;
 
+import android.animation.ArgbEvaluator;
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.TabLayout;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.graphics.Palette;
 
+import com.charlesmadere.hummingbird.R;
 import com.facebook.drawee.backends.pipeline.Fresco;
 import com.facebook.drawee.controller.AbstractDraweeController;
 import com.facebook.drawee.view.SimpleDraweeView;
+import com.facebook.imagepipeline.request.BasePostprocessor;
 import com.facebook.imagepipeline.request.ImageRequest;
 import com.facebook.imagepipeline.request.ImageRequestBuilder;
+
+import java.lang.ref.WeakReference;
 
 public final class PaletteUtils {
 
@@ -29,4 +38,75 @@ public final class PaletteUtils {
         simpleDraweeView.setController(controller);
     }
 
+
+    private static final class PalettePostprocessor extends BasePostprocessor implements
+            Palette.PaletteAsyncListener {
+
+        private static final long TRANSITION_TIME_MS = 250L;
+
+        private final ArgbEvaluator mArgbEvaluator;
+        private final WeakReference<Activity> mActivity;
+        private final WeakReference<AppBarLayout> mAppBarLayout;
+        private final WeakReference<CollapsingToolbarLayout> mCollapsingToolbarLayout;
+        private final WeakReference<TabLayout> mTabLayout;
+
+
+        private PalettePostprocessor(final Activity activity, final AppBarLayout appBarLayout,
+                final CollapsingToolbarLayout collapsingToolbarLayout, final TabLayout tabLayout) {
+            mActivity = new WeakReference<>(activity);
+            mAppBarLayout = new WeakReference<>(appBarLayout);
+            mCollapsingToolbarLayout = new WeakReference<>(collapsingToolbarLayout);
+            mTabLayout = new WeakReference<>(tabLayout);
+            mArgbEvaluator = new ArgbEvaluator();
+        }
+
+        private boolean isAlive() {
+            final Activity activity = mActivity.get();
+            return activity != null && !activity.isDestroyed() && mAppBarLayout.get() != null &&
+                    mCollapsingToolbarLayout.get() != null && mTabLayout.get() != null;
+        }
+
+        @Override
+        public void onGenerated(final Palette palette) {
+            if (!isAlive()) {
+                return;
+            }
+
+            final AppBarLayout appBarLayout = mAppBarLayout.get();
+            final CollapsingToolbarLayout collapsingToolbarLayout = mCollapsingToolbarLayout.get();
+            final TabLayout tabLayout = mTabLayout.get();
+
+            if (appBarLayout == null || collapsingToolbarLayout == null || tabLayout == null) {
+                return;
+            }
+
+            final Context context = tabLayout.getContext();
+            final int darkMutedColor = palette.getDarkMutedColor(ContextCompat.getColor(context,
+                    R.color.colorPrimaryDark));
+            final int darkVibrantColor = palette.getDarkVibrantColor(ContextCompat.getColor(context,
+                    R.color.colorPrimary));
+            final int vibrantColor = palette.getVibrantColor(ContextCompat.getColor(context,
+                    R.color.colorAccent));
+
+            // TODO use ValueAnimator
+            // https://stackoverflow.com/questions/2614545/animate-change-of-view-background-color-in-android
+
+            appBarLayout.setBackgroundColor(darkVibrantColor);
+            collapsingToolbarLayout.setStatusBarScrimColor(darkMutedColor);
+            collapsingToolbarLayout.setContentScrimColor(darkVibrantColor);
+            tabLayout.setSelectedTabIndicatorColor(vibrantColor);
+        }
+
+        @Override
+        public void process(final Bitmap bitmap) {
+            super.process(bitmap);
+
+            if (!isAlive()) {
+                return;
+            }
+
+            Palette.from(bitmap).generate(this);
+        }
+
+    }
 }
