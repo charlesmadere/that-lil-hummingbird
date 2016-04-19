875bd2b6bfd4ec661be088ad0df1395bcb38cf59
charlesmadere <charlesrmadere@gmail.com>
2016-04-19 00:02:22 -0700

    the progress bar is now a better color

--- project/app/src/main/java/com/charlesmadere/hummingbird/fragments/GalleryFragment.java
+++ project/app/src/main/java/com/charlesmadere/hummingbird/fragments/GalleryFragment.java
@@ -2,6 +2,7 @@ package com.charlesmadere.hummingbird.fragments;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +13,10 @@ import com.charlesmadere.hummingbird.models.GalleryImage;
 import com.facebook.drawee.backends.pipeline.Fresco;
 import com.facebook.drawee.controller.AbstractDraweeController;
 import com.facebook.drawee.controller.BaseControllerListener;
+import com.facebook.drawee.drawable.ProgressBarDrawable;
+import com.facebook.drawee.drawable.ScalingUtils;
+import com.facebook.drawee.generic.GenericDraweeHierarchy;
+import com.facebook.drawee.generic.GenericDraweeHierarchyBuilder;
 import com.facebook.drawee.view.SimpleDraweeView;
 import com.facebook.imagepipeline.image.ImageInfo;
 import com.facebook.imagepipeline.request.ImageRequest;
@@ -66,6 +71,16 @@ public class GalleryFragment extends BaseFragment {
     @Override
     public void onViewCreated(final View view, final Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        final ProgressBarDrawable progressBarDrawable = new ProgressBarDrawable();
+        progressBarDrawable.setColor(ContextCompat.getColor(getContext(), R.color.colorAccent));
+
+        final GenericDraweeHierarchy hierarchy = new GenericDraweeHierarchyBuilder(getResources())
+                .setActualImageScaleType(ScalingUtils.ScaleType.CENTER_INSIDE)
+                .setProgressBarImage(progressBarDrawable)
+                .build();
+
+        mImage.setHierarchy(hierarchy);
 
         final ImageRequest request = ImageRequestBuilder.newBuilderWithSource(
                 Uri.parse(mGalleryImage.getOriginal())).build();
