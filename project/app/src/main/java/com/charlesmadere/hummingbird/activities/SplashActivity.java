1258441c6c21bafd0e5391512408200f626e269a
charlesmadere <charlesrmadere@gmail.com>
2016-04-25 00:30:27 -0700

    filled in logic for splash activity

--- project/app/src/main/java/com/charlesmadere/hummingbird/activities/SplashActivity.java
+++ project/app/src/main/java/com/charlesmadere/hummingbird/activities/SplashActivity.java
@@ -3,6 +3,7 @@ package com.charlesmadere.hummingbird.activities;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.ImageView;
 
 import com.charlesmadere.hummingbird.R;
@@ -15,6 +16,10 @@ public class SplashActivity extends BaseActivity {
     private static final String TAG = "SplashActivity";
     private static final String EXTRA_FINISH = CNAME + ".Finish";
 
+    private static final long ANIM_TIME_MS = 1500L;
+
+
+    private Handler mHandler;
 
     @Bind(R.id.ivSplash)
     ImageView mSplash;
@@ -33,6 +38,61 @@ public class SplashActivity extends BaseActivity {
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        mHandler = new Handler();
+
+        final Runnable four = new Runnable() {
+            @Override
+            public void run() {
+                if (isDestroyed()) {
+                    return;
+                }
+
+                final Intent intent = getIntent();
+
+                if (intent == null || !intent.getBooleanExtra(EXTRA_FINISH, false)) {
+                    startActivity(LoginActivity.getLaunchIntent(SplashActivity.this));
+                }
+
+                finish();
+            }
+        };
+
+        final Runnable three = new Runnable() {
+            @Override
+            public void run() {
+                if (isDestroyed()) {
+                    return;
+                }
+
+                mSplash.setImageResource(R.drawable.three);
+                mHandler.postDelayed(four, ANIM_TIME_MS);
+            }
+        };
+
+        final Runnable two = new Runnable() {
+            @Override
+            public void run() {
+                if (isDestroyed()) {
+                    return;
+                }
+
+                mSplash.setImageResource(R.drawable.two);
+                mHandler.postDelayed(three, ANIM_TIME_MS);
+            }
+        };
+
+        mHandler.postDelayed(two, ANIM_TIME_MS);
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mHandler != null) {
+            mHandler.removeCallbacksAndMessages(null);
+            mHandler = null;
+        }
+
+        super.onDestroy();
     }
 
 }
