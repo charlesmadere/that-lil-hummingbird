0be904a617652f829ddad6e1c40b53f4f2214cd9
charlesmadere <charlesrmadere@gmail.com>
2016-04-30 14:39:05 -0700

    beginning filling of the Feed class

--- project/app/src/main/java/com/charlesmadere/hummingbird/models/Feed.java
+++ project/app/src/main/java/com/charlesmadere/hummingbird/models/Feed.java
@@ -3,10 +3,17 @@ package com.charlesmadere.hummingbird.models;
 import android.os.Parcel;
 import android.os.Parcelable;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Feed implements Parcelable {
 
-    // TODO
+    @SerializedName("meta")
+    private Metadata mMetadata;
 
+
+    public Metadata getMetadata() {
+        return mMetadata;
+    }
 
     @Override
     public int describeContents() {
@@ -21,8 +28,9 @@ public class Feed implements Parcelable {
     public static final Creator<Feed> CREATOR = new Creator<Feed>() {
         @Override
         public Feed createFromParcel(final Parcel source) {
+            final Feed f = new Feed();
             // TODO
-            return null;
+            return f;
         }
 
         @Override
@@ -30,5 +38,40 @@ public class Feed implements Parcelable {
             return new Feed[size];
         }
     };
+
+
+    private static class Metadata implements Parcelable {
+        @SerializedName("cursor")
+        private int mCursor;
+
+
+        public int getCursor() {
+            return mCursor;
+        }
+
+        @Override
+        public int describeContents() {
+            return 0;
+        }
+
+        @Override
+        public void writeToParcel(final Parcel dest, final int flags) {
+            dest.writeInt(mCursor);
+        }
+
+        public static final Creator<Metadata> CREATOR = new Creator<Metadata>() {
+            @Override
+            public Metadata createFromParcel(final Parcel source) {
+                final Metadata m = new Metadata();
+                m.mCursor = source.readInt();
+                return m;
+            }
+
+            @Override
+            public Metadata[] newArray(final int size) {
+                return new Metadata[size];
+            }
+        };
+    }
 
 }
