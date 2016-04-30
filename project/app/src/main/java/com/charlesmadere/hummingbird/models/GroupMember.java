fbba05f5139cfd61b9cea2b1ab0a3bbe216db433
charlesmadere <charlesrmadere@gmail.com>
2016-04-30 16:19:37 -0700

    filled in the GroupMember class

--- project/app/src/main/java/com/charlesmadere/hummingbird/models/GroupMember.java
+++ project/app/src/main/java/com/charlesmadere/hummingbird/models/GroupMember.java
@@ -3,10 +3,45 @@ package com.charlesmadere.hummingbird.models;
 import android.os.Parcel;
 import android.os.Parcelable;
 
+import com.google.gson.annotations.SerializedName;
+
 public class GroupMember implements Parcelable {
 
-    // TODO
+    @SerializedName("pending")
+    private boolean mPending;
 
+    @SerializedName("group_id")
+    private String mGroupId;
+
+    @SerializedName("id")
+    private String mId;
+
+    @SerializedName("rank")
+    private String mRank;
+
+    @SerializedName("user_id")
+    private String mUserId;
+
+
+    public String getGroupId() {
+        return mGroupId;
+    }
+
+    public String getId() {
+        return mId;
+    }
+
+    public String getRank() {
+        return mRank;
+    }
+
+    public String getUserId() {
+        return mUserId;
+    }
+
+    public boolean isPending() {
+        return mPending;
+    }
 
     @Override
     public int describeContents() {
@@ -15,14 +50,22 @@ public class GroupMember implements Parcelable {
 
     @Override
     public void writeToParcel(final Parcel dest, final int flags) {
-        // TODO
+        dest.writeInt(mPending ? 1 : 0);
+        dest.writeString(mGroupId);
+        dest.writeString(mId);
+        dest.writeString(mRank);
+        dest.writeString(mUserId);
     }
 
     public static final Creator<GroupMember> CREATOR = new Creator<GroupMember>() {
         @Override
         public GroupMember createFromParcel(final Parcel source) {
             final GroupMember gm = new GroupMember();
-            // TODO
+            gm.mPending = source.readInt() != 0;
+            gm.mGroupId = source.readString();
+            gm.mId = source.readString();
+            gm.mRank = source.readString();
+            gm.mUserId = source.readString();
             return gm;
         }
 
