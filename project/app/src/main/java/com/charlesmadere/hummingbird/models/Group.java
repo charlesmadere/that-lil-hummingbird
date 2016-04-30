17d4b0c895efc41e0dffe656ca624aad5caf33c1
charlesmadere <charlesrmadere@gmail.com>
2016-04-30 16:10:28 -0700

    filled in the Group class

--- project/app/src/main/java/com/charlesmadere/hummingbird/models/Group.java
+++ project/app/src/main/java/com/charlesmadere/hummingbird/models/Group.java
@@ -3,10 +3,73 @@ package com.charlesmadere.hummingbird.models;
 import android.os.Parcel;
 import android.os.Parcelable;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Group implements Parcelable {
 
-    // TODO
+    @SerializedName("member_count")
+    private int mMemberCount;
 
+    @SerializedName("about")
+    private String mAbout;
+
+    @SerializedName("about_formatted")
+    private String mAboutFormatted;
+
+    @SerializedName("avatar_url")
+    private String mAvatarUrl;
+
+    @SerializedName("bio")
+    private String mBio;
+
+    @SerializedName("cover_image_url")
+    private String mCoverImageUrl;
+
+    @SerializedName("current_member_id")
+    private String mCurrentMemberId;
+
+    @SerializedName("id")
+    private String mId;
+
+    @SerializedName("name")
+    private String mName;
+
+
+    public String getAbout() {
+        return mAbout;
+    }
+
+    public String getAboutFormatted() {
+        return mAboutFormatted;
+    }
+
+    public String getAvatarUrl() {
+        return mAvatarUrl;
+    }
+
+    public String getBio() {
+        return mBio;
+    }
+
+    public String getCoverImageUrl() {
+        return mCoverImageUrl;
+    }
+
+    public String getCurrentMemberId() {
+        return mCurrentMemberId;
+    }
+
+    public String getId() {
+        return mId;
+    }
+
+    public int getMemberCount() {
+        return mMemberCount;
+    }
+
+    public String getName() {
+        return mName;
+    }
 
     @Override
     public int describeContents() {
@@ -15,14 +78,30 @@ public class Group implements Parcelable {
 
     @Override
     public void writeToParcel(final Parcel dest, final int flags) {
-        // TODO
+        dest.writeInt(mMemberCount);
+        dest.writeString(mAbout);
+        dest.writeString(mAboutFormatted);
+        dest.writeString(mAvatarUrl);
+        dest.writeString(mBio);
+        dest.writeString(mCoverImageUrl);
+        dest.writeString(mCurrentMemberId);
+        dest.writeString(mId);
+        dest.writeString(mName);
     }
 
     public static final Creator<Group> CREATOR = new Creator<Group>() {
         @Override
         public Group createFromParcel(final Parcel source) {
             final Group g = new Group();
-            // TODO
+            g.mMemberCount = source.readInt();
+            g.mAbout = source.readString();
+            g.mAboutFormatted = source.readString();
+            g.mAvatarUrl = source.readString();
+            g.mBio = source.readString();
+            g.mCoverImageUrl = source.readString();
+            g.mCurrentMemberId = source.readString();
+            g.mId = source.readString();
+            g.mName = source.readString();
             return g;
         }
 
