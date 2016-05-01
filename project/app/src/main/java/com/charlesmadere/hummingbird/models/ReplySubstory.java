bbeed65d0f86881bf543b0e16a2e58017f6cb4a8
charlesmadere <charlesrmadere@gmail.com>
2016-05-01 16:22:50 -0700

    created new ReplySubstory class

---
+++ project/app/src/main/java/com/charlesmadere/hummingbird/models/ReplySubstory.java
@@ -0,0 +1,71 @@
+package com.charlesmadere.hummingbird.models;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.google.gson.annotations.SerializedName;
+
+public class ReplySubstory extends AbsSubstory implements Parcelable {
+
+    @SerializedName("reply")
+    private String mReply;
+
+    @SerializedName("user_id")
+    private String mUserId;
+
+    @SerializedName("user")
+    private User mUser;
+
+
+    public String getReply() {
+        return mReply;
+    }
+
+    @Override
+    public Type getType() {
+        return Type.REPLY;
+    }
+
+    public User getUser() {
+        return mUser;
+    }
+
+    public String getUserId() {
+        return mUserId;
+    }
+
+    public void setUser(final User user) {
+        mUser = user;
+    }
+
+    @Override
+    protected void readFromParcel(final Parcel source) {
+        super.readFromParcel(source);
+        mReply = source.readString();
+        mUserId = source.readString();
+        mUser = source.readParcelable(User.class.getClassLoader());
+    }
+
+    @Override
+    public void writeToParcel(final Parcel dest, final int flags) {
+        super.writeToParcel(dest, flags);
+        dest.writeString(mReply);
+        dest.writeString(mUserId);
+        dest.writeParcelable(mUser, flags);
+    }
+
+    public static final Creator<ReplySubstory> CREATOR = new Creator<ReplySubstory>() {
+        @Override
+        public ReplySubstory createFromParcel(final Parcel source) {
+            final ReplySubstory rs = new ReplySubstory();
+            rs.readFromParcel(source);
+            return rs;
+        }
+
+        @Override
+        public ReplySubstory[] newArray(final int size) {
+            return new ReplySubstory[size];
+        }
+    };
+
+}
