4c35cd7f299202e5b3a5e78f6dcc4638a0da0e20
charlesmadere <charlesrmadere@gmail.com>
2016-05-01 16:12:11 -0700

    created new WatchlistStatusUpdateSubstory class

---
+++ project/app/src/main/java/com/charlesmadere/hummingbird/models/WatchlistStatusUpdateSubstory.java
@@ -0,0 +1,91 @@
+package com.charlesmadere.hummingbird.models;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.google.gson.annotations.SerializedName;
+
+public class WatchlistStatusUpdateSubstory extends WatchedEpisodeSubstory implements Parcelable {
+
+    @SerializedName("new_status")
+    private NewStatus mNewStatus;
+
+
+    public NewStatus getNewStatus() {
+        return mNewStatus;
+    }
+
+    @Override
+    public Type getType() {
+        return Type.WATCHLIST_STATUS_UPDATE;
+    }
+
+    @Override
+    protected void readFromParcel(final Parcel source) {
+        super.readFromParcel(source);
+        mNewStatus = source.readParcelable(NewStatus.class.getClassLoader());
+    }
+
+    @Override
+    public void writeToParcel(final Parcel dest, final int flags) {
+        super.writeToParcel(dest, flags);
+        dest.writeParcelable(mNewStatus, flags);
+    }
+
+    public static final Creator<WatchlistStatusUpdateSubstory> CREATOR = new Creator<WatchlistStatusUpdateSubstory>() {
+        @Override
+        public WatchlistStatusUpdateSubstory createFromParcel(final Parcel source) {
+            final WatchlistStatusUpdateSubstory wsus = new WatchlistStatusUpdateSubstory();
+            wsus.readFromParcel(source);
+            return wsus;
+        }
+
+        @Override
+        public WatchlistStatusUpdateSubstory[] newArray(final int size) {
+            return new WatchlistStatusUpdateSubstory[size];
+        }
+    };
+
+
+    public enum NewStatus implements Parcelable {
+        @SerializedName("completed")
+        COMPLETED,
+
+        @SerializedName("currently_watching")
+        CURRENTLY_WATCHING,
+
+        @SerializedName("dropped")
+        DROPPED,
+
+        @SerializedName("on_hold")
+        ON_HOLD,
+
+        @SerializedName("plan_to_watch")
+        PLAN_TO_WATCH;
+
+
+        @Override
+        public int describeContents() {
+            return 0;
+        }
+
+        @Override
+        public void writeToParcel(final Parcel dest, final int flags) {
+            dest.writeInt(ordinal());
+        }
+
+        public static final Creator<NewStatus> CREATOR = new Creator<NewStatus>() {
+            @Override
+            public NewStatus createFromParcel(final Parcel source) {
+                final int ordinal = source.readInt();
+                return values()[ordinal];
+            }
+
+            @Override
+            public NewStatus[] newArray(final int size) {
+                return new NewStatus[size];
+            }
+        };
+    }
+
+}
