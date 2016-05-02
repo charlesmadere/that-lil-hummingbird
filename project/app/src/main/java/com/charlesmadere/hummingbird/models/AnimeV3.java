8bac8aa400778788791199ebb7141ffd1f2a1cce
charlesmadere <charlesrmadere@gmail.com>
2016-05-01 21:47:15 -0700

    added mGenres field to AnimeV3 class

--- project/app/src/main/java/com/charlesmadere/hummingbird/models/AnimeV3.java
+++ project/app/src/main/java/com/charlesmadere/hummingbird/models/AnimeV3.java
@@ -4,8 +4,19 @@ import android.content.res.Resources;
 import android.os.Parcel;
 import android.os.Parcelable;
 import android.support.annotation.Nullable;
+import android.text.TextUtils;
+
+import com.charlesmadere.hummingbird.R;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
 
 public class AnimeV3 extends AbsAnime implements Parcelable {
+
+    @Nullable
+    @SerializedName("genres")
+    private ArrayList<String> mGenres;
+
 
     @Nullable
     @Override
@@ -15,7 +26,11 @@ public class AnimeV3 extends AbsAnime implements Parcelable {
 
     @Override
     public String getGenresString(final Resources res) {
-        return null;
+        if (!hasGenres()) {
+            return "";
+        }
+
+        return TextUtils.join(res.getText(R.string.delimiter), mGenres);
     }
 
     @Nullable
@@ -36,7 +51,19 @@ public class AnimeV3 extends AbsAnime implements Parcelable {
 
     @Override
     public boolean hasGenres() {
-        return false;
+        return mGenres != null && !mGenres.isEmpty();
+    }
+
+    @Override
+    protected void readFromParcel(final Parcel source) {
+        super.readFromParcel(source);
+        mGenres = source.createStringArrayList();
+    }
+
+    @Override
+    public void writeToParcel(final Parcel dest, final int flags) {
+        super.writeToParcel(dest, flags);
+        dest.writeStringList(mGenres);
     }
 
     public static final Creator<AnimeV3> CREATOR = new Creator<AnimeV3>() {
