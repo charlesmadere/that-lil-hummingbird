fb4255e757e03e931c80e150e0c22b279f5478f1
charlesmadere <charlesrmadere@gmail.com>
2016-05-01 21:57:14 -0700

    created new TitleType class

---
+++ project/app/src/main/java/com/charlesmadere/hummingbird/models/TitleType.java
@@ -0,0 +1,59 @@
+package com.charlesmadere.hummingbird.models;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.support.annotation.StringRes;
+
+import com.charlesmadere.hummingbird.R;
+import com.google.gson.annotations.SerializedName;
+
+public enum TitleType implements Parcelable {
+
+    @SerializedName("canonical")
+    CANONICAL(R.string.canonical),
+
+    @SerializedName("english")
+    ENGLISH(R.string.english),
+
+    @SerializedName("japanese")
+    JAPANESE(R.string.japanese),
+
+    @SerializedName("romaji")
+    ROMAJI(R.string.romaji);
+
+    private final int mTextResId;
+
+
+    TitleType(@StringRes final int textResId) {
+        mTextResId = textResId;
+    }
+
+    @StringRes
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(final Parcel dest, final int flags) {
+        dest.writeInt(ordinal());
+    }
+
+    public static final Creator<TitleType> CREATOR = new Creator<TitleType>() {
+        @Override
+        public TitleType createFromParcel(final Parcel source) {
+            final int ordinal = source.readInt();
+            return values()[ordinal];
+        }
+
+        @Override
+        public TitleType[] newArray(final int size) {
+            return new TitleType[size];
+        }
+    };
+
+}
