9de6f17be1643f6d4453784fdc71568f906e92c5
charlesmadere <charlesrmadere@gmail.com>
2016-05-08 15:08:38 -0700

    beginning writing a custom view for the anime breakdown

---
+++ project/app/src/main/java/com/charlesmadere/hummingbird/views/AnimeBreakdownPieView.java
@@ -0,0 +1,60 @@
+package com.charlesmadere.hummingbird.views;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.os.Build;
+import android.support.v4.content.ContextCompat;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.charlesmadere.hummingbird.R;
+
+public class AnimeBreakdownPieView extends View {
+
+    private float mBarThickness;
+    private int mPrimaryColor;
+    private int mSecondaryColor;
+
+
+    public AnimeBreakdownPieView(final Context context, final AttributeSet attrs) {
+        super(context, attrs);
+        parseAttributes(attrs);
+    }
+
+    public AnimeBreakdownPieView(final Context context, final AttributeSet attrs,
+            final int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        parseAttributes(attrs);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public AnimeBreakdownPieView(final Context context, final AttributeSet attrs,
+            final int defStyleAttr, final int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        parseAttributes(attrs);
+    }
+
+    @Override
+    protected void onMeasure(final int widthMeasureSpec, final int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, widthMeasureSpec);
+    }
+
+    private void parseAttributes(final AttributeSet attrs) {
+        if (isInEditMode()) {
+            return;
+        }
+
+        final Context context = getContext();
+        final TypedArray ta = context.obtainStyledAttributes(attrs,
+                R.styleable.AnimeBreakdownPieView);
+        mBarThickness = ta.getDimension(R.styleable.AnimeBreakdownPieView_barThickness,
+                getResources().getDimension(R.dimen.root_padding));
+        mPrimaryColor = ta.getColor(R.styleable.AnimeBreakdownPieView_primaryColor,
+                ContextCompat.getColor(context, R.color.orange));
+        mSecondaryColor = ta.getColor(R.styleable.AnimeBreakdownPieView_secondaryColor,
+                ContextCompat.getColor(context, R.color.orangeTranslucent));
+        ta.recycle();
+    }
+
+}
---
+++ project/app/src/main/java/com/charlesmadere/hummingbird/views/AnimeBreakdownView.java
@@ -0,0 +1,71 @@
+package com.charlesmadere.hummingbird.views;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.charlesmadere.hummingbird.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class AnimeBreakdownView extends LinearLayout {
+
+    @BindView(R.id.animeBreakdownPieView)
+    AnimeBreakdownPieView mAnimeBreakdownPieView;
+
+    @BindView(R.id.kvtvGenre0)
+    KeyValueTextView mGenre0;
+
+    @BindView(R.id.kvtvGenre1)
+    KeyValueTextView mGenre1;
+
+    @BindView(R.id.kvtvGenre2)
+    KeyValueTextView mGenre2;
+
+    @BindView(R.id.kvtvGenre3)
+    KeyValueTextView mGenre3;
+
+    @BindView(R.id.kvtvGenre4)
+    KeyValueTextView mGenre4;
+
+    @BindView(R.id.kvtvGenre5)
+    KeyValueTextView mGenre5;
+
+    @BindView(R.id.tvGenreCount)
+    TextView mGenreCount;
+
+    @BindView(R.id.tvPrimaryGenre)
+    TextView mPrimaryGenre;
+
+
+    public AnimeBreakdownView(final Context context, final AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public AnimeBreakdownView(final Context context, final AttributeSet attrs,
+            final int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public AnimeBreakdownView(final Context context, final AttributeSet attrs,
+            final int defStyleAttr, final int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+
+        if (isInEditMode()) {
+            return;
+        }
+
+        ButterKnife.bind(this);
+    }
+
+}
