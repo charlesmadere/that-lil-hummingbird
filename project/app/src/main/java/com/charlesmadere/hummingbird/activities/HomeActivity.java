1064d0f51c1279b8ceec2cbfe5f1b15a19a41c10
charlesmadere <charlesrmadere@gmail.com>
2016-05-09 23:31:18 -0700

    HomeActivity now utilizes HomeFragmentAdapter

--- project/app/src/main/java/com/charlesmadere/hummingbird/activities/HomeActivity.java
+++ project/app/src/main/java/com/charlesmadere/hummingbird/activities/HomeActivity.java
@@ -3,46 +3,24 @@ package com.charlesmadere.hummingbird.activities;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.widget.SwipeRefreshLayout;
-import android.support.v7.widget.RecyclerView;
-import android.view.View;
-import android.widget.LinearLayout;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 
 import com.charlesmadere.hummingbird.R;
-import com.charlesmadere.hummingbird.adapters.FeedAdapter;
-import com.charlesmadere.hummingbird.models.ErrorInfo;
-import com.charlesmadere.hummingbird.models.Feed;
-import com.charlesmadere.hummingbird.networking.Api;
-import com.charlesmadere.hummingbird.networking.ApiResponse;
+import com.charlesmadere.hummingbird.adapters.HomeFragmentAdapter;
 import com.charlesmadere.hummingbird.views.NavigationDrawerItemView;
-import com.charlesmadere.hummingbird.views.RefreshLayout;
-import com.charlesmadere.hummingbird.views.SpaceItemDecoration;
-
-import java.lang.ref.WeakReference;
 
 import butterknife.BindView;
 
-public class HomeActivity extends BaseDrawerActivity implements
-        SwipeRefreshLayout.OnRefreshListener {
+public class HomeActivity extends BaseDrawerActivity {
 
     private static final String TAG = "HomeActivity";
-    private static final String KEY_FEED = "Feed";
 
-    private Feed mFeed;
-    private FeedAdapter mAdapter;
+    @BindView(R.id.tabLayout)
+    TabLayout mTabLayout;
 
-    @BindView(R.id.llEmpty)
-    LinearLayout mEmpty;
-
-    @BindView(R.id.llError)
-    LinearLayout mError;
-
-    @BindView(R.id.recyclerView)
-    RecyclerView mRecyclerView;
-
-    @BindView(R.id.refreshLayout)
-    RefreshLayout mRefreshLayout;
+    @BindView(R.id.viewPager)
+    ViewPager mViewPager;
 
 
     public static Intent getLaunchIntent(final Context context) {
@@ -53,11 +31,6 @@ public class HomeActivity extends BaseDrawerActivity implements
 
     public static Intent getNewTaskLaunchIntent(final Context context) {
         return getLaunchIntent(context).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-    }
-
-    private void fetchFeed() {
-        mRefreshLayout.setRefreshing(true);
-        Api.getNewsFeed(new GetFeedListener(this));
     }
 
     @Override
@@ -75,93 +48,10 @@ public class HomeActivity extends BaseDrawerActivity implements
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        if (savedInstanceState != null && !savedInstanceState.isEmpty()) {
-            mFeed = savedInstanceState.getParcelable(KEY_FEED);
-        }
-
-        if (mFeed == null) {
-            fetchFeed();
-        } else {
-            showFeed(mFeed);
-        }
-    }
-
-    @Override
-    public void onRefresh() {
-        fetchFeed();
-    }
-
-    @Override
-    protected void onSaveInstanceState(final Bundle outState) {
-        super.onSaveInstanceState(outState);
-
-        if (mFeed != null) {
-            outState.putParcelable(KEY_FEED, mFeed);
-        }
-    }
-
-    @Override
-    protected void onViewsBound() {
-        super.onViewsBound();
-
-        mRefreshLayout.setOnRefreshListener(this);
-        mAdapter = new FeedAdapter(this);
-        mRecyclerView.setAdapter(mAdapter);
-        SpaceItemDecoration.apply(mRecyclerView, false, R.dimen.root_padding);
-    }
-
-    private void showEmpty() {
-        mRecyclerView.setVisibility(View.GONE);
-        mError.setVisibility(View.GONE);
-        mEmpty.setVisibility(View.VISIBLE);
-        mRefreshLayout.setRefreshing(false);
-    }
-
-    private void showError() {
-        mRecyclerView.setVisibility(View.GONE);
-        mEmpty.setVisibility(View.GONE);
-        mError.setVisibility(View.VISIBLE);
-        mRefreshLayout.setRefreshing(false);
-    }
-
-    private void showFeed(final Feed feed) {
-        mFeed = feed;
-        mAdapter.set(mFeed);
-        mEmpty.setVisibility(View.GONE);
-        mError.setVisibility(View.GONE);
-        mRecyclerView.setVisibility(View.VISIBLE);
-        mRefreshLayout.setRefreshing(false);
-    }
-
-
-    private static class GetFeedListener implements ApiResponse<Feed> {
-        private final WeakReference<HomeActivity> mActivityReference;
-
-        private GetFeedListener(final HomeActivity activity) {
-            mActivityReference = new WeakReference<>(activity);
-        }
-
-        @Override
-        public void failure(@Nullable final ErrorInfo error) {
-            final HomeActivity activity = mActivityReference.get();
-
-            if (activity != null && !activity.isDestroyed()) {
-                activity.showError();
-            }
-        }
-
-        @Override
-        public void success(final Feed feed) {
-            final HomeActivity activity = mActivityReference.get();
-
-            if (activity != null && !activity.isDestroyed()) {
-                if (feed.hasStories()) {
-                    activity.showFeed(feed);
-                } else {
-                    activity.showEmpty();
-                }
-            }
-        }
+        mViewPager.setAdapter(new HomeFragmentAdapter(this));
+        mViewPager.setPageMargin(getResources().getDimensionPixelSize(R.dimen.root_padding));
+        mViewPager.setOffscreenPageLimit(3);
+        mTabLayout.setupWithViewPager(mViewPager);
     }
 
 }
