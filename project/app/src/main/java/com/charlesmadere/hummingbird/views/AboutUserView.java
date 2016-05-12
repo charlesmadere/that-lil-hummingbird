6ce150fac7579c1d9853a7bdd2156319a96cf8d5
charlesmadere <charlesrmadere@gmail.com>
2016-05-11 23:43:42 -0700

    added views

--- project/app/src/main/java/com/charlesmadere/hummingbird/views/AboutUserView.java
+++ project/app/src/main/java/com/charlesmadere/hummingbird/views/AboutUserView.java
@@ -3,10 +3,21 @@ package com.charlesmadere.hummingbird.views;
 import android.content.Context;
 import android.support.v7.widget.CardView;
 import android.util.AttributeSet;
+import android.widget.TextView;
 
+import com.charlesmadere.hummingbird.R;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class AboutUserView extends CardView {
+
+    @BindView(R.id.tvAbout)
+    TextView mAbout;
+
+    @BindView(R.id.tvBio)
+    TextView mBio;
+
 
     public AboutUserView(final Context context, final AttributeSet attrs) {
         super(context, attrs);
