d7fcbebcd6e3eb60b468df2a11754a9a6acb1b3f
charlesmadere <charlesrmadere@gmail.com>
2016-05-12 22:57:00 -0700

    filled in the views for NotificationActivity

--- project/app/src/main/java/com/charlesmadere/hummingbird/activities/NotificationsActivity.java
+++ project/app/src/main/java/com/charlesmadere/hummingbird/activities/NotificationsActivity.java
@@ -3,13 +3,30 @@ package com.charlesmadere.hummingbird.activities;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.widget.LinearLayout;
 
 import com.charlesmadere.hummingbird.R;
 import com.charlesmadere.hummingbird.views.NavigationDrawerItemView;
+import com.charlesmadere.hummingbird.views.RefreshLayout;
+
+import butterknife.BindView;
 
 public class NotificationsActivity extends BaseDrawerActivity {
 
     private static final String TAG = "NotificationsActivity";
+
+    @BindView(R.id.llEmpty)
+    LinearLayout mEmpty;
+
+    @BindView(R.id.llError)
+    LinearLayout mError;
+
+    @BindView(R.id.recyclerView)
+    RecyclerView mRecyclerView;
+
+    @BindView(R.id.refreshLayout)
+    RefreshLayout mRefreshLayout;
 
 
     public static Intent getLaunchIntent(final Context context) {
