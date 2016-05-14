9e47bc45ac1e8804d2f9e57cc8017446e9094cca
charlesmadere <charlesrmadere@gmail.com>
2016-05-14 14:47:55 -0700

    filled in lots of the logic for NotificationsActivity

--- project/app/src/main/java/com/charlesmadere/hummingbird/activities/NotificationsActivity.java
+++ project/app/src/main/java/com/charlesmadere/hummingbird/activities/NotificationsActivity.java
@@ -3,18 +3,31 @@ package com.charlesmadere.hummingbird.activities;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.LinearLayout;
 
 import com.charlesmadere.hummingbird.R;
+import com.charlesmadere.hummingbird.models.ErrorInfo;
+import com.charlesmadere.hummingbird.models.Feed;
+import com.charlesmadere.hummingbird.networking.Api;
+import com.charlesmadere.hummingbird.networking.ApiResponse;
 import com.charlesmadere.hummingbird.views.NavigationDrawerItemView;
 import com.charlesmadere.hummingbird.views.RefreshLayout;
 
+import java.lang.ref.WeakReference;
+
 import butterknife.BindView;
 
-public class NotificationsActivity extends BaseDrawerActivity {
+public class NotificationsActivity extends BaseDrawerActivity implements
+        SwipeRefreshLayout.OnRefreshListener {
 
     private static final String TAG = "NotificationsActivity";
+    private static final String KEY_FEED = "Feed";
+
+    private Feed mFeed;
 
     @BindView(R.id.llEmpty)
     LinearLayout mEmpty;
@@ -33,6 +46,11 @@ public class NotificationsActivity extends BaseDrawerActivity {
         return new Intent(context, NotificationsActivity.class);
     }
 
+    private void fetchNotifications() {
+        mRefreshLayout.setRefreshing(true);
+        Api.getNotifications(new GetNotificationsListener(this));
+    }
+
     @Override
     public String getActivityName() {
         return TAG;
@@ -47,6 +65,89 @@ public class NotificationsActivity extends BaseDrawerActivity {
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notifications);
+
+        if (savedInstanceState != null && !savedInstanceState.isEmpty()) {
+            mFeed = savedInstanceState.getParcelable(KEY_FEED);
+        }
+
+        if (mFeed == null) {
+            fetchNotifications();
+        } else {
+            showFeed(mFeed);
+        }
+    }
+
+    @Override
+    public void onRefresh() {
+        fetchNotifications();
+    }
+
+    @Override
+    protected void onSaveInstanceState(final Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        if (mFeed != null) {
+            outState.putParcelable(KEY_FEED, mFeed);
+        }
+    }
+
+    @Override
+    protected void onViewsBound() {
+        super.onViewsBound();
+        mRefreshLayout.setOnRefreshListener(this);
+    }
+
+    private void showEmpty() {
+        mRecyclerView.setVisibility(View.GONE);
+        mError.setVisibility(View.GONE);
+        mEmpty.setVisibility(View.VISIBLE);
+        mRefreshLayout.setRefreshing(false);
+    }
+
+    private void showError() {
+        mRecyclerView.setVisibility(View.GONE);
+        mEmpty.setVisibility(View.GONE);
+        mError.setVisibility(View.VISIBLE);
+        mRefreshLayout.setRefreshing(false);
+    }
+
+    private void showFeed(final Feed feed) {
+        mFeed = feed;
+        mEmpty.setVisibility(View.GONE);
+        mError.setVisibility(View.GONE);
+        mRecyclerView.setVisibility(View.VISIBLE);
+        mRefreshLayout.setRefreshing(false);
+    }
+
+
+    private static class GetNotificationsListener implements ApiResponse<Feed> {
+        private final WeakReference<NotificationsActivity> mActivityReference;
+
+        private GetNotificationsListener(final NotificationsActivity activity) {
+            mActivityReference = new WeakReference<>(activity);
+        }
+
+        @Override
+        public void failure(@Nullable final ErrorInfo error) {
+            final NotificationsActivity activity = mActivityReference.get();
+
+            if (activity != null && !activity.isDestroyed()) {
+                activity.showError();
+            }
+        }
+
+        @Override
+        public void success(final Feed feed) {
+            final NotificationsActivity activity = mActivityReference.get();
+
+            if (activity != null && !activity.isDestroyed()) {
+                if (feed.hasStories()) {
+                    activity.showFeed(feed);
+                } else {
+                    activity.showEmpty();
+                }
+            }
+        }
     }
 
 }
