c5bc56a5d5e6a8e9b02f9073b31ec39aeb400fc4
Charles Madere <charlesrmadere@gmail.com>
2016-05-14 01:04:41 -0700

    reply view is now set

--- project/app/src/main/java/com/charlesmadere/hummingbird/views/ReplySubstoryStandaloneItemView.java
+++ project/app/src/main/java/com/charlesmadere/hummingbird/views/ReplySubstoryStandaloneItemView.java
@@ -1,15 +1,34 @@
 package com.charlesmadere.hummingbird.views;
 
 import android.content.Context;
+import android.net.Uri;
 import android.support.v7.widget.CardView;
 import android.util.AttributeSet;
+import android.widget.TextView;
 
+import com.charlesmadere.hummingbird.R;
+import com.charlesmadere.hummingbird.activities.UserActivity;
 import com.charlesmadere.hummingbird.adapters.AdapterView;
 import com.charlesmadere.hummingbird.models.ReplySubstory;
+import com.facebook.drawee.view.SimpleDraweeView;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class ReplySubstoryStandaloneItemView extends CardView implements AdapterView<ReplySubstory> {
+
+    private ReplySubstory mReplySubstory;
+
+    @BindView(R.id.kvtvReply)
+    KeyValueTextView mReply;
+
+    @BindView(R.id.sdvAvatar)
+    SimpleDraweeView mAvatar;
+
+    @BindView(R.id.tvTimeAgo)
+    TextView mTimeAgo;
+
 
     public ReplySubstoryStandaloneItemView(final Context context, final AttributeSet attrs) {
         super(context, attrs);
@@ -18,6 +37,12 @@ public class ReplySubstoryStandaloneItemView extends CardView implements Adapter
     public ReplySubstoryStandaloneItemView(final Context context, final AttributeSet attrs,
             final int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+    }
+
+    @OnClick(R.id.sdvAvatar)
+    void onAvatarClick() {
+        final Context context = getContext();
+        context.startActivity(UserActivity.getLaunchIntent(context, mReplySubstory.getUser()));
     }
 
     @Override
@@ -33,7 +58,10 @@ public class ReplySubstoryStandaloneItemView extends CardView implements Adapter
 
     @Override
     public void setContent(final ReplySubstory content) {
-
+        mReplySubstory = content;
+        mAvatar.setImageURI(Uri.parse(content.getUser().getAvatarSmall()));
+        mReply.setText(mReplySubstory.getUser().getName(), mReplySubstory.getReply());
+        mTimeAgo.setText(mReplySubstory.getCreatedAt().getRelativeTimeText(getContext()));
     }
 
 }
