0005cad6298da572c1ef511e5cf9364868b85675
charlesmadere <charlesrmadere@gmail.com>
2016-05-14 16:19:06 -0700

    implemented click listener functionality for profile comment notification item vierw

--- project/app/src/main/java/com/charlesmadere/hummingbird/views/ProfileCommentNotificationItemView.java
+++ project/app/src/main/java/com/charlesmadere/hummingbird/views/ProfileCommentNotificationItemView.java
@@ -3,9 +3,14 @@ package com.charlesmadere.hummingbird.views;
 import android.content.Context;
 import android.support.v7.widget.CardView;
 import android.util.AttributeSet;
+import android.widget.TextView;
 
 import com.charlesmadere.hummingbird.R;
+import com.charlesmadere.hummingbird.activities.UserActivity;
 import com.charlesmadere.hummingbird.adapters.AdapterView;
+import com.charlesmadere.hummingbird.models.AbsNotification;
+import com.charlesmadere.hummingbird.models.AbsStory;
+import com.charlesmadere.hummingbird.models.CommentStory;
 import com.charlesmadere.hummingbird.models.ProfileCommentNotification;
 import com.facebook.drawee.view.SimpleDraweeView;
 
@@ -18,8 +23,14 @@ public class ProfileCommentNotificationItemView extends CardView implements
 
     private ProfileCommentNotification mProfileCommentNotification;
 
+    @BindView(R.id.notificationTitleTextView)
+    NotificationTitleTextView mNotificationTitleTextView;
+
     @BindView(R.id.sdvAvatar)
     SimpleDraweeView mAvatar;
+
+    @BindView(R.id.tvTimeAgo)
+    TextView mTimeAgo;
 
 
     public ProfileCommentNotificationItemView(final Context context, final AttributeSet attrs) {
@@ -31,9 +42,35 @@ public class ProfileCommentNotificationItemView extends CardView implements
         super(context, attrs, defStyleAttr);
     }
 
+    private void handleStoryAvatarClick(final AbsNotification.StorySource source) {
+        final AbsStory story = source.getStory();
+        final Context context = getContext();
+
+        switch (story.getType()) {
+            case COMMENT:
+                context.startActivity(UserActivity.getLaunchIntent(context,
+                        ((CommentStory) story).getPoster()));
+                break;
+
+            default:
+                throw new RuntimeException("encountered unknown " + AbsStory.Type.class.getName()
+                        + ": \"" + story.getType() + '"');
+        }
+    }
+
     @OnClick(R.id.sdvAvatar)
     void onAvatarClick() {
+        final AbsNotification.AbsSource source = mProfileCommentNotification.getSource();
 
+        switch (source.getType()) {
+            case STORY:
+                handleStoryAvatarClick((AbsNotification.StorySource) source);
+                break;
+
+            default:
+                throw new RuntimeException("encountered unknown " +
+                        AbsNotification.AbsSource.Type.class.getName() + ": \"" + source + '"');
+        }
     }
 
     @Override
