fdfd38cabb2317cba8d8d2d0fd5a3aa807692305
charlesmadere <charlesrmadere@gmail.com>
2016-05-21 21:08:34 -0700

    deleted unused constant

--- project/app/src/main/java/com/charlesmadere/hummingbird/misc/Constants.java
+++ project/app/src/main/java/com/charlesmadere/hummingbird/misc/Constants.java
@@ -2,7 +2,6 @@ package com.charlesmadere.hummingbird.misc;
 
 public final class Constants {
 
-    public static final String API_KEY = "314377a803c797adb1f1";
     public static final String BASE_API_URL= "https://hummingbird.me/";
     public static final String CHARLES_TWITTER_URL = "https://twitter.com/charlesmadere";
     public static final String GITHUB_URL = "https://github.com/charlesmadere/that-lil-hummingbird";
