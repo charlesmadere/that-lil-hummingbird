3c54e903fce06416edb52970c373b1238149da77
charlesmadere <charlesrmadere@gmail.com>
2016-05-28 14:09:05 -0700

    the cast is now sorted! :)

--- project/app/src/main/java/com/charlesmadere/hummingbird/adapters/AnimeCastingsAdapter.java
+++ project/app/src/main/java/com/charlesmadere/hummingbird/adapters/AnimeCastingsAdapter.java
@@ -1,17 +1,33 @@
 package com.charlesmadere.hummingbird.adapters;
 
 import android.content.Context;
+import android.text.TextUtils;
 
 import com.charlesmadere.hummingbird.R;
 import com.charlesmadere.hummingbird.models.AnimeDigest;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.TreeMap;
 
-public class AnimeCastingsAdapter extends BaseMultiAdapter {
+public class AnimeCastingsAdapter extends BaseMultiAdapter implements Comparator<String> {
 
     public AnimeCastingsAdapter(final Context context) {
         super(context);
+    }
+
+    @Override
+    public int compare(final String lhs, final String rhs) {
+        if (TextUtils.isEmpty(lhs) && TextUtils.isEmpty(rhs)) {
+            return 0;
+        } else if (TextUtils.isEmpty(lhs)) {
+            return Integer.MAX_VALUE;
+        } else if (TextUtils.isEmpty(rhs)) {
+            return Integer.MIN_VALUE;
+        } else {
+            return lhs.compareToIgnoreCase(rhs);
+        }
     }
 
     @Override
@@ -23,7 +39,7 @@ public class AnimeCastingsAdapter extends BaseMultiAdapter {
     }
 
     public void set(final ArrayList<AnimeDigest.Casting> castings) {
-        final HashMap<String, ArrayList<AnimeDigest.Casting>> map = new HashMap<>();
+        final TreeMap<String, ArrayList<AnimeDigest.Casting>> map = new TreeMap<>(this);
 
         for (final AnimeDigest.Casting casting : castings) {
             final String language = casting.getLanguage();
@@ -38,7 +54,12 @@ public class AnimeCastingsAdapter extends BaseMultiAdapter {
         final ArrayList<Object> list = new ArrayList<>();
 
         for (final String language : map.keySet()) {
-            list.add(language);
+            if (TextUtils.isEmpty(language)) {
+                list.add(getContext().getString(R.string.other));
+            } else {
+                list.add(language);
+            }
+
             list.addAll(map.get(language));
         }
 
