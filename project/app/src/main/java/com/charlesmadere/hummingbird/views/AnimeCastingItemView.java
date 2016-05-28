55c6eec9d0e07018072438424a59f13582218f30
charlesmadere <charlesrmadere@gmail.com>
2016-05-28 12:37:40 -0700

    filled in view details

--- project/app/src/main/java/com/charlesmadere/hummingbird/views/AnimeCastingItemView.java
+++ project/app/src/main/java/com/charlesmadere/hummingbird/views/AnimeCastingItemView.java
@@ -4,10 +4,25 @@ import android.content.Context;
 import android.support.v7.widget.CardView;
 import android.util.AttributeSet;
 
+import com.charlesmadere.hummingbird.R;
 import com.charlesmadere.hummingbird.adapters.AdapterView;
 import com.charlesmadere.hummingbird.models.AnimeDigest;
+import com.facebook.drawee.view.SimpleDraweeView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class AnimeCastingItemView extends CardView implements AdapterView<AnimeDigest.Casting> {
+
+    @BindView(R.id.kvtvTitle)
+    KeyValueTextView mTitle;
+
+    @BindView(R.id.sdvCastPhoto)
+    SimpleDraweeView mCastPhoto;
+
+    @BindView(R.id.sdvCharacterPhoto)
+    SimpleDraweeView mCharacterPhoto;
+
 
     public AnimeCastingItemView(final Context context, final AttributeSet attrs) {
         super(context, attrs);
@@ -16,6 +31,12 @@ public class AnimeCastingItemView extends CardView implements AdapterView<AnimeD
     public AnimeCastingItemView(final Context context, final AttributeSet attrs,
             final int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        ButterKnife.bind(this);
     }
 
     @Override
