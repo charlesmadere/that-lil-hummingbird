e2229a19dcff385d1994ba55afbcb4c09bd77ca0
charlesmadere <charlesrmadere@gmail.com>
2016-06-05 18:30:49 -0700

    GroupDetailsFragment now fills in its views

--- project/app/src/main/java/com/charlesmadere/hummingbird/fragments/GroupDetailsFragment.java
+++ project/app/src/main/java/com/charlesmadere/hummingbird/fragments/GroupDetailsFragment.java
@@ -4,9 +4,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.charlesmadere.hummingbird.R;
+import com.charlesmadere.hummingbird.models.Group;
 import com.charlesmadere.hummingbird.models.GroupDigest;
+
+import java.text.NumberFormat;
+
+import butterknife.BindView;
+import butterknife.OnClick;
 
 public class GroupDetailsFragment extends BaseFragment {
 
@@ -14,6 +21,27 @@ public class GroupDetailsFragment extends BaseFragment {
     private static final String KEY_GROUP_DIGEST = "GroupDigest";
 
     private GroupDigest mGroupDigest;
+
+    @BindView(R.id.tvAboutBody)
+    TextView mAboutBody;
+
+    @BindView(R.id.tvAboutTitle)
+    TextView mAboutTitle;
+
+    @BindView(R.id.tvBioBody)
+    TextView mBioBody;
+
+    @BindView(R.id.tvBioTitle)
+    TextView mBioTitle;
+
+    @BindView(R.id.tvMembersBody)
+    TextView mMembersBody;
+
+    @BindView(R.id.tvMembersHeader)
+    TextView mMembersHeader;
+
+    @BindView(R.id.tvNoBio)
+    TextView mNoBio;
 
 
     public static GroupDetailsFragment create(final GroupDigest digest) {
@@ -29,6 +57,11 @@ public class GroupDetailsFragment extends BaseFragment {
     @Override
     public String getFragmentName() {
         return TAG;
+    }
+
+    @OnClick(R.id.llMembers)
+    void onMembersClick() {
+        // TODO
     }
 
     @Override
@@ -50,6 +83,27 @@ public class GroupDetailsFragment extends BaseFragment {
     public void onViewCreated(final View view, final Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        final Group group = mGroupDigest.getGroup();
+        mAboutTitle.setText(getString(R.string.about_x, group.getName()));
+
+        if (group.hasAbout()) {
+            mAboutBody.setText(group.getAbout());
+            mAboutBody.setVisibility(View.VISIBLE);
+        }
+
+        final NumberFormat numberFormat = NumberFormat.getNumberInstance();
+        mMembersHeader.setText(numberFormat.format(group.getMemberCount()));
+        mMembersBody.setText(getResources().getQuantityText(R.plurals.members,
+                group.getMemberCount()));
+
+        mBioTitle.setText(getString(R.string.bio_for_x, group.getName()));
+
+        if (group.hasBio()) {
+            mBioBody.setText(group.getBio());
+            mBioBody.setVisibility(View.VISIBLE);
+        } else {
+            mNoBio.setVisibility(View.VISIBLE);
+        }
     }
 
 }
