74c443018805e67193706bfaf2ef9e6cb38ca7da
charlesmadere <charlesrmadere@gmail.com>
2016-06-12 23:25:09 -0700

    added method

--- project/app/src/main/java/com/charlesmadere/hummingbird/fragments/FeedPostDialogFragment.java
+++ project/app/src/main/java/com/charlesmadere/hummingbird/fragments/FeedPostDialogFragment.java
@@ -3,6 +3,9 @@ package com.charlesmadere.hummingbird.fragments;
 public class FeedPostDialogFragment extends BaseDialogFragment {
 
 
+    public static FeedPostDialogFragment create() {
+        return new FeedPostDialogFragment();
+    }
 
     public String getFeedPost() {
         return "";
