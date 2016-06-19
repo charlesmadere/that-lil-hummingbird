e00931463ab4c920fa0fb0e3fc9c5a177f07281e
charlesmadere <charlesrmadere@gmail.com>
2016-06-18 17:07:50 -0700

    re-added dumb RefreshLayout workaround

--- project/app/src/main/java/com/charlesmadere/hummingbird/views/RefreshLayout.java
+++ project/app/src/main/java/com/charlesmadere/hummingbird/views/RefreshLayout.java
@@ -82,4 +82,13 @@ public class RefreshLayout extends SwipeRefreshLayout {
         ta.recycle();
     }
 
+    @Override
+    public void setRefreshing(final boolean refreshing) {
+        post(new Runnable() {
+            @Override
+            public void run() {
+                RefreshLayout.super.setRefreshing(refreshing);
+            }
+        });
+    }
 }
