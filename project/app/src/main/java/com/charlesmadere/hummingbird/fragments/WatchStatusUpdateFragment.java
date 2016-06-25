32e6ceb8e32a272f618260cdb6ce98b523d1c102
charlesmadere <charlesrmadere@gmail.com>
2016-06-25 15:29:04 -0700

    added some layout elements to WatchStatusUpdateFragment

--- project/app/src/main/java/com/charlesmadere/hummingbird/fragments/WatchStatusUpdateFragment.java
+++ project/app/src/main/java/com/charlesmadere/hummingbird/fragments/WatchStatusUpdateFragment.java
@@ -4,12 +4,19 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import com.charlesmadere.hummingbird.R;
+
+import butterknife.BindView;
+import butterknife.OnClick;
 
 public class WatchStatusUpdateFragment extends BaseBottomSheetDialogFragment {
 
     private static final String TAG = "WatchStatusUpdateFragment";
+
+    @BindView(R.id.ibSave)
+    ImageButton mSave;
 
 
     public static WatchStatusUpdateFragment create() {
@@ -26,6 +33,11 @@ public class WatchStatusUpdateFragment extends BaseBottomSheetDialogFragment {
         return TAG;
     }
 
+    @OnClick(R.id.ibClose)
+    void onCloseClick() {
+        dismiss();
+    }
+
     @Override
     public void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,6 +51,11 @@ public class WatchStatusUpdateFragment extends BaseBottomSheetDialogFragment {
             final Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         return inflater.inflate(R.layout.fragment_watch_status_update, container, false);
+    }
+
+    @OnClick(R.id.ibSave)
+    void onSaveClick() {
+
     }
 
 }
