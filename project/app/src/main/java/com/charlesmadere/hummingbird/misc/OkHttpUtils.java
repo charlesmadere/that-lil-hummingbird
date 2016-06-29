0da00f4afb15bc22dcb16b182215340477f557d4
charlesmadere <charlesrmadere@gmail.com>
2016-06-28 21:52:07 -0700

    added okhttp logging

--- project/app/src/main/java/com/charlesmadere/hummingbird/misc/OkHttpUtils.java
+++ project/app/src/main/java/com/charlesmadere/hummingbird/misc/OkHttpUtils.java
@@ -4,6 +4,7 @@ import com.charlesmadere.hummingbird.ThatLilHummingbird;
 
 import okhttp3.Cache;
 import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 
 public final class OkHttpUtils {
 
@@ -16,8 +17,13 @@ public final class OkHttpUtils {
     public static synchronized OkHttpClient getOkHttpClient() {
         if (sOkHttpClient == null) {
             Timber.d(TAG, "Creating OkHttpClient instance");
+
+            final HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+            interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
             sOkHttpClient = new OkHttpClient.Builder()
                     .cache(new Cache(ThatLilHummingbird.get().getCacheDir(), CACHE_MAX_SIZE))
+                    .addInterceptor(interceptor)
                     .build();
         }
 
