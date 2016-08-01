dff7d65378ebcb86ba25f944de2be5a42fa56598
Charles Madere <charlesrmadere@gmail.com>
2016-07-31 21:59:27 -0700

    working on restyling app news items

--- project/app/src/main/java/com/charlesmadere/hummingbird/views/AppNewsItemView.java
+++ project/app/src/main/java/com/charlesmadere/hummingbird/views/AppNewsItemView.java
@@ -1,13 +1,11 @@
 package com.charlesmadere.hummingbird.views;
 
-import android.annotation.TargetApi;
 import android.content.Context;
-import android.os.Build;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.CardView;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.charlesmadere.hummingbird.R;
@@ -19,7 +17,7 @@ import java.text.NumberFormat;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class AppNewsItemView extends LinearLayout implements AdapterView<AppNews> {
+public class AppNewsItemView extends CardView implements AdapterView<AppNews> {
 
     private AppNews mAppNews;
     private NumberFormat mNumberFormat;
@@ -47,12 +45,6 @@ public class AppNewsItemView extends LinearLayout implements AdapterView<AppNews
     public AppNewsItemView(final Context context, final AttributeSet attrs,
             final int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-    }
-
-    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-    public AppNewsItemView(final Context context, final AttributeSet attrs, final int defStyleAttr,
-            final int defStyleRes) {
-        super(context, attrs, defStyleAttr, defStyleRes);
     }
 
     public AppNews getAppNews() {
