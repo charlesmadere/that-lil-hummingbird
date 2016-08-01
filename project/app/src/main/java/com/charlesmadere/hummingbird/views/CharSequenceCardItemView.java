5864eb5ba24ccec6c3b99ca9ce19ce2b12f25562
Charles Madere <charlesrmadere@gmail.com>
2016-07-31 23:41:41 -0700

    reworking section header style

---
+++ project/app/src/main/java/com/charlesmadere/hummingbird/views/CharSequenceCardItemView.java
@@ -0,0 +1,40 @@
+package com.charlesmadere.hummingbird.views;
+
+import android.content.Context;
+import android.support.v7.widget.CardView;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+import com.charlesmadere.hummingbird.R;
+import com.charlesmadere.hummingbird.adapters.AdapterView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class CharSequenceCardItemView extends CardView implements AdapterView<CharSequence> {
+
+    @BindView(R.id.tvTitle)
+    TextView mTitle;
+
+
+    public CharSequenceCardItemView(final Context context, final AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CharSequenceCardItemView(final Context context, final AttributeSet attrs,
+            final int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        ButterKnife.bind(this);
+    }
+
+    @Override
+    public void setContent(final CharSequence content) {
+        mTitle.setText(content);
+    }
+
+}
