e00337ddbff573a7afda76754c3cee1f551dcc05
Charles Madere <charlesrmadere@gmail.com>
2016-08-15 19:38:56 -0700

    BaseBottomSheetDialogFragment will now auto expand

--- project/app/src/main/java/com/charlesmadere/hummingbird/fragments/BaseBottomSheetDialogFragment.java
+++ project/app/src/main/java/com/charlesmadere/hummingbird/fragments/BaseBottomSheetDialogFragment.java
@@ -1,8 +1,14 @@
 package com.charlesmadere.hummingbird.fragments;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.BottomSheetBehavior;
 import android.support.design.widget.BottomSheetDialogFragment;
+import android.support.design.widget.CoordinatorLayout;
 import android.view.View;
+import android.view.ViewGroup;
 
 import com.charlesmadere.hummingbird.misc.Timber;
 
@@ -17,6 +23,62 @@ public abstract class BaseBottomSheetDialogFragment extends BottomSheetDialogFra
     private Unbinder mUnbinder;
 
 
+    private void autoExpand() {
+        final CoordinatorLayout coordinatorLayout = findCoordinatorLayout(
+                getDialog().getWindow().getDecorView());
+
+        if (coordinatorLayout == null) {
+            return;
+        }
+
+        View view = null;
+
+        for (int i = 0; i < coordinatorLayout.getChildCount(); ++i) {
+            view = coordinatorLayout.getChildAt(i);
+
+            if (view instanceof ViewGroup) {
+                break;
+            }
+        }
+
+        if (view == null) {
+            return;
+        }
+
+        final ViewGroup.LayoutParams vglp = view.getLayoutParams();
+
+        if (vglp instanceof CoordinatorLayout.LayoutParams) {
+            final CoordinatorLayout.LayoutParams cllp = (CoordinatorLayout.LayoutParams)
+                    view.getLayoutParams();
+            final BottomSheetBehavior behavior = BottomSheetBehavior.from(view);
+            behavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+            cllp.setBehavior(behavior);
+            view.setLayoutParams(cllp);
+        }
+    }
+
+    @Nullable
+    private CoordinatorLayout findCoordinatorLayout(@Nullable final View view) {
+        if (view instanceof CoordinatorLayout) {
+            return (CoordinatorLayout) view;
+        } else if (view instanceof ViewGroup) {
+            final ViewGroup viewGroup = (ViewGroup) view;
+
+            for (int i = 0; i < viewGroup.getChildCount(); ++i) {
+                final View v = viewGroup.getChildAt(i);
+                final CoordinatorLayout coordinatorLayout = findCoordinatorLayout(v);
+
+                if (coordinatorLayout != null) {
+                    return coordinatorLayout;
+                }
+            }
+
+            return null;
+        } else {
+            return null;
+        }
+    }
+
     public abstract String getFragmentName();
 
     public boolean isDestroyed() {
@@ -27,6 +89,20 @@ public abstract class BaseBottomSheetDialogFragment extends BottomSheetDialogFra
     public void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Timber.d(TAG, '"' + getFragmentName() + "\" created");
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final Dialog dialog = super.onCreateDialog(savedInstanceState);
+
+        dialog.setOnShowListener(new DialogInterface.OnShowListener() {
+            @Override
+            public void onShow(final DialogInterface dialog) {
+                autoExpand();
+            }
+        });
+
+        return dialog;
     }
 
     @Override
