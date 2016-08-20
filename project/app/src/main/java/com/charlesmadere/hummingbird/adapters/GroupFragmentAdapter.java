12ef24aa22e950bc9caeffdf4d9d44813be7b15b
Charles Madere <charlesrmadere@gmail.com>
2016-08-20 16:05:17 -0700

    GroupFragmentAdapter now holds on to the fragments

--- project/app/src/main/java/com/charlesmadere/hummingbird/adapters/GroupFragmentAdapter.java
+++ project/app/src/main/java/com/charlesmadere/hummingbird/adapters/GroupFragmentAdapter.java
@@ -5,14 +5,23 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.util.SparseArrayCompat;
+import android.view.ViewGroup;
 
 import com.charlesmadere.hummingbird.R;
+import com.charlesmadere.hummingbird.fragments.BaseFragment;
 import com.charlesmadere.hummingbird.fragments.GroupDetailsFragment;
 import com.charlesmadere.hummingbird.fragments.GroupFeedFragment;
 
+import java.lang.ref.WeakReference;
+
 public class GroupFragmentAdapter extends FragmentStatePagerAdapter {
 
+    public static final int POSITION_FEED = 0;
+    public static final int POSITION_DETAILS = 1;
+
     private final Context mContext;
+    private final SparseArrayCompat<WeakReference<BaseFragment>> mFragments;
 
 
     public GroupFragmentAdapter(final FragmentActivity activity) {
@@ -22,6 +31,13 @@ public class GroupFragmentAdapter extends FragmentStatePagerAdapter {
     public GroupFragmentAdapter(final Context context, final FragmentManager fm) {
         super(fm);
         mContext = context;
+        mFragments = new SparseArrayCompat<>(getCount());
+    }
+
+    @Override
+    public void destroyItem(final ViewGroup container, final int position, final Object object) {
+        super.destroyItem(container, position, object);
+        mFragments.removeAt(position);
     }
 
     @Override
@@ -32,8 +48,8 @@ public class GroupFragmentAdapter extends FragmentStatePagerAdapter {
     @Override
     public Fragment getItem(final int position) {
         switch (position) {
-            case 0: return GroupFeedFragment.create();
-            case 1: return GroupDetailsFragment.create();
+            case POSITION_FEED: return GroupFeedFragment.create();
+            case POSITION_DETAILS: return GroupDetailsFragment.create();
             default: throw new IllegalArgumentException("illegal position: " + position);
         }
     }
@@ -41,10 +57,17 @@ public class GroupFragmentAdapter extends FragmentStatePagerAdapter {
     @Override
     public CharSequence getPageTitle(final int position) {
         switch (position) {
-            case 0: return mContext.getText(R.string.feed);
-            case 1: return mContext.getText(R.string.details);
+            case POSITION_FEED: return mContext.getText(R.string.feed);
+            case POSITION_DETAILS: return mContext.getText(R.string.details);
             default: throw new IllegalArgumentException("illegal position: " + position);
         }
+    }
+
+    @Override
+    public BaseFragment instantiateItem(final ViewGroup container, final int position) {
+        final BaseFragment fragment = (BaseFragment) super.instantiateItem(container, position);
+        mFragments.put(position, new WeakReference<>(fragment));
+        return fragment;
     }
 
 }
