c031a4c44b35345cba89e8cf3d5eee8f77ffc733
Charles Madere <charlesrmadere@gmail.com>
2016-09-15 17:58:39 -0700

    created new FollowedStoryActivity and FollowedStoryAdapter

---
+++ project/app/src/main/java/com/charlesmadere/hummingbird/activities/FollowedStoryActivity.java
@@ -0,0 +1,222 @@
+package com.charlesmadere.hummingbird.activities;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.charlesmadere.hummingbird.R;
+import com.charlesmadere.hummingbird.adapters.FollowedStoryAdapter;
+import com.charlesmadere.hummingbird.misc.ObjectCache;
+import com.charlesmadere.hummingbird.models.ErrorInfo;
+import com.charlesmadere.hummingbird.models.Feed;
+import com.charlesmadere.hummingbird.models.FollowedStory;
+import com.charlesmadere.hummingbird.models.UiColorSet;
+import com.charlesmadere.hummingbird.networking.Api;
+import com.charlesmadere.hummingbird.networking.ApiResponse;
+import com.charlesmadere.hummingbird.views.RecyclerViewPaginator;
+import com.charlesmadere.hummingbird.views.RefreshLayout;
+
+import java.lang.ref.WeakReference;
+
+import butterknife.BindView;
+
+public class FollowedStoryActivity extends BaseDrawerActivity implements ObjectCache.KeyProvider,
+        RecyclerViewPaginator.Listeners, SwipeRefreshLayout.OnRefreshListener {
+
+    private static final String TAG = "FollowedStoryActivity";
+    private static final String CNAME = FollowedStoryActivity.class.getCanonicalName();
+    private static final String EXTRA_FOLLOWED_STORY = CNAME + ".FollowedStory";
+
+    private Feed mFeed;
+    private FollowedStory mFollowedStory;
+    private FollowedStoryAdapter mAdapter;
+    private RecyclerViewPaginator mPaginator;
+
+    @BindView(R.id.llError)
+    LinearLayout mError;
+
+    @BindView(R.id.recyclerView)
+    RecyclerView mRecyclerView;
+
+    @BindView(R.id.refreshLayout)
+    RefreshLayout mRefreshLayout;
+
+
+    public static Intent getLaunchIntent(final Context context, final FollowedStory followedStory) {
+        return getLaunchIntent(context, followedStory, null);
+    }
+
+    public static Intent getLaunchIntent(final Context context, final FollowedStory followedStory,
+            @Nullable final UiColorSet uiColorSet) {
+        final Intent intent = new Intent(context, MediaStoryActivity.class)
+                .putExtra(EXTRA_FOLLOWED_STORY, followedStory);
+
+        if (uiColorSet != null) {
+            intent.putExtra(EXTRA_UI_COLOR_SET, uiColorSet);
+        }
+
+        return intent;
+    }
+
+    private void fetchFeed() {
+        mRefreshLayout.setRefreshing(true);
+        Api.getSubstories(mFollowedStory.getId(), new GetSubstoriesListener(this));
+    }
+
+    @Override
+    public String getActivityName() {
+        return TAG;
+    }
+
+    @Override
+    public String[] getObjectCacheKeys() {
+        return new String[] { getActivityName(), mFollowedStory.getId() };
+    }
+
+    @Override
+    public boolean isLoading() {
+        return mRefreshLayout.isRefreshing() || mAdapter.isPaginating();
+    }
+
+    @Override
+    protected boolean isUpNavigationEnabled() {
+        return true;
+    }
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_followed_story);
+
+        final Intent intent = getIntent();
+        mFollowedStory = intent.getParcelableExtra(EXTRA_FOLLOWED_STORY);
+
+        mFeed = ObjectCache.get(this);
+
+        if (mFeed == null) {
+            fetchFeed();
+        } else {
+            showFeed(mFeed);
+        }
+    }
+
+    @Override
+    public void onRefresh() {
+        fetchFeed();
+    }
+
+    @Override
+    protected void onSaveInstanceState(final Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        if (mFeed != null && mFeed.hasSubstories()) {
+            ObjectCache.put(mFeed, this);
+        }
+    }
+
+    @Override
+    protected void onViewsBound() {
+        super.onViewsBound();
+        mRefreshLayout.setOnRefreshListener(this);
+        mRecyclerView.setHasFixedSize(true);
+        mAdapter = new FollowedStoryAdapter(this);
+        mRecyclerView.setAdapter(mAdapter);
+        mPaginator = new RecyclerViewPaginator(mRecyclerView, this);
+    }
+
+    @Override
+    public void paginate() {
+        mAdapter.setPaginating(true);
+        Api.getSubstories(mFollowedStory.getId(), mFeed, new PaginateFeedListener(this));
+    }
+
+    private void paginationComplete() {
+        mAdapter.set(mFollowedStory, mFeed);
+        mAdapter.setPaginating(false);
+    }
+
+    private void paginationNoMore() {
+        mPaginator.setEnabled(false);
+        mAdapter.setPaginating(false);
+    }
+
+    private void showError() {
+        mRecyclerView.setVisibility(View.GONE);
+        mError.setVisibility(View.VISIBLE);
+        mRefreshLayout.setRefreshing(false);
+    }
+
+    private void showFeed(final Feed feed) {
+        mFeed = feed;
+        mAdapter.set(mFollowedStory, mFeed);
+        mError.setVisibility(View.GONE);
+        mRecyclerView.setVisibility(View.VISIBLE);
+        mPaginator.setEnabled(feed.hasCursor());
+        mRefreshLayout.setRefreshing(false);
+    }
+
+
+    private static class GetSubstoriesListener implements ApiResponse<Feed> {
+        private final WeakReference<FollowedStoryActivity> mActivityReference;
+
+        private GetSubstoriesListener(final FollowedStoryActivity activity) {
+            mActivityReference = new WeakReference<>(activity);
+        }
+
+        @Override
+        public void failure(@Nullable final ErrorInfo error) {
+            final FollowedStoryActivity activity = mActivityReference.get();
+
+            if (activity != null && activity.isAlive()) {
+                activity.showError();
+            }
+        }
+
+        @Override
+        public void success(final Feed feed) {
+            final FollowedStoryActivity activity = mActivityReference.get();
+
+            if (activity != null && activity.isAlive()) {
+                activity.showFeed(feed);
+            }
+        }
+    }
+
+    private static class PaginateFeedListener implements ApiResponse<Feed> {
+        private final WeakReference<FollowedStoryActivity> mActivityReference;
+        private final int mSubstoriesSize;
+
+        private PaginateFeedListener(final FollowedStoryActivity activity) {
+            mActivityReference = new WeakReference<>(activity);
+            mSubstoriesSize = activity.mFeed.getSubstoriesSize();
+        }
+
+        @Override
+        public void failure(@Nullable final ErrorInfo error) {
+            final FollowedStoryActivity activity = mActivityReference.get();
+
+            if (activity != null && activity.isAlive()) {
+                activity.paginationNoMore();
+            }
+        }
+
+        @Override
+        public void success(final Feed feed) {
+            final FollowedStoryActivity activity = mActivityReference.get();
+
+            if (activity != null && activity.isAlive()) {
+                if (feed.hasCursor() && feed.getSubstoriesSize() > mSubstoriesSize) {
+                    activity.paginationComplete();
+                } else {
+                    activity.paginationNoMore();
+                }
+            }
+        }
+    }
+
+}
---
+++ project/app/src/main/java/com/charlesmadere/hummingbird/adapters/FollowedStoryAdapter.java
@@ -0,0 +1,57 @@
+package com.charlesmadere.hummingbird.adapters;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+
+import com.charlesmadere.hummingbird.R;
+import com.charlesmadere.hummingbird.models.Feed;
+import com.charlesmadere.hummingbird.models.FollowedStory;
+import com.charlesmadere.hummingbird.models.FollowedSubstory;
+import com.charlesmadere.hummingbird.models.ReplySubstory;
+import com.charlesmadere.hummingbird.views.ReplySubstoryStandaloneItemView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class FollowedStoryAdapter extends BaseMultiPaginationAdapter {
+
+    public FollowedStoryAdapter(final Context context) {
+        super(context);
+    }
+
+    @Override
+    protected HashMap<Class, Integer> getItemViewKeyMap() {
+        final HashMap<Class, Integer> map = new HashMap<>(3);
+        map.put(FollowedStory.class, R.layout.item_followed_story_standalone);
+        map.put(FollowedSubstory.class, R.layout.item_followed_substory_standalone);
+        map.put(String.class, R.layout.item_charsequence_plain);
+        return map;
+    }
+
+    @Override
+    public void onBindViewHolder(final AdapterView.ViewHolder holder, final int position) {
+        if (holder.getAdapterView() instanceof ReplySubstoryStandaloneItemView) {
+            final boolean showDivider = isPaginating() ? position + 2 < getItemCount()
+                    : position + 1 < getItemCount();
+
+            ((ReplySubstoryStandaloneItemView) holder.getAdapterView()).setContent(
+                    (ReplySubstory) getItem(position), showDivider);
+        } else {
+            super.onBindViewHolder(holder, position);
+        }
+    }
+
+    public void set(final FollowedStory followedStory, @Nullable final Feed feed) {
+        final ArrayList<Object> list = new ArrayList<>();
+        list.add(followedStory);
+
+        if (feed == null || !feed.hasSubstories()) {
+            list.add(getContext().getString(R.string.nothing_more_to_show));
+        } else {
+            list.addAll(feed.getSubstories());
+        }
+
+        super.set(list);
+    }
+
+}
