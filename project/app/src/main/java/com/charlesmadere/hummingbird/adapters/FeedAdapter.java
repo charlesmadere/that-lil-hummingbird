4fe7471335c38120783b48938682c818b257fe5e
Charles Madere <charlesrmadere@gmail.com>
2016-09-21 19:10:29 -0700

    FeedAdapter now has stable IDs

--- project/app/src/main/java/com/charlesmadere/hummingbird/adapters/FeedAdapter.java
+++ project/app/src/main/java/com/charlesmadere/hummingbird/adapters/FeedAdapter.java
@@ -13,6 +13,16 @@ public class FeedAdapter extends BasePaginationAdapter<AbsStory> {
 
     public FeedAdapter(final Context context) {
         super(context);
+        setHasStableIds(true);
+    }
+
+    @Override
+    public long getItemId(final int position) {
+        if (isPaginating() && position == getItemCount() - 1) {
+            return Long.MIN_VALUE;
+        } else {
+            return getItem(position).hashCode();
+        }
     }
 
     @Override
