af45546d073cc145b2c2568060c1cc85f47a8c66
Charles Madere <charlesrmadere@gmail.com>
2016-10-30 11:43:57 -0700

    views are now bound and click listeners set

--- project/app/src/main/java/com/charlesmadere/hummingbird/fragments/FavoriteAnimeFragment.java
+++ project/app/src/main/java/com/charlesmadere/hummingbird/fragments/FavoriteAnimeFragment.java
@@ -1,15 +1,26 @@
 package com.charlesmadere.hummingbird.fragments;
 
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.charlesmadere.hummingbird.R;
 
-public class FavoriteAnimeFragment extends BaseBottomSheetDialogFragment {
+import butterknife.BindView;
+
+public class FavoriteAnimeFragment extends BaseBottomSheetDialogFragment implements
+        View.OnClickListener {
 
     private static final String TAG = "FavoriteAnimeFragment";
+
+    @BindView(R.id.recyclerView)
+    RecyclerView mRecyclerView;
+
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
 
 
     public static FavoriteAnimeFragment create() {
@@ -22,10 +33,24 @@ public class FavoriteAnimeFragment extends BaseBottomSheetDialogFragment {
     }
 
     @Override
+    public void onClick(final View view) {
+        dismissAllowingStateLoss();
+    }
+
+    @Override
     public View onCreateView(final LayoutInflater inflater, final ViewGroup container,
             final Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         return inflater.inflate(R.layout.fragment_favorite_anime, container, false);
+    }
+
+    @Override
+    public void onViewCreated(final View view, final Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mToolbar.setNavigationIcon(R.drawable.ic_close_white_24dp);
+        mToolbar.setNavigationOnClickListener(this);
+        mToolbar.setTitle(R.string.favorite_anime);
     }
 
 }
