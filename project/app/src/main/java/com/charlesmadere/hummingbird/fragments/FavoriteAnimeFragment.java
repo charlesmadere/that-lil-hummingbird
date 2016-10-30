7506caecbefc20a2fad608116e980b9467d00dc8
Charles Madere <charlesrmadere@gmail.com>
2016-10-30 00:15:29 -0700

    begin creation of layout for FavoriteAnimeFragment

--- project/app/src/main/java/com/charlesmadere/hummingbird/fragments/FavoriteAnimeFragment.java
+++ project/app/src/main/java/com/charlesmadere/hummingbird/fragments/FavoriteAnimeFragment.java
@@ -1,5 +1,12 @@
 package com.charlesmadere.hummingbird.fragments;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.charlesmadere.hummingbird.R;
+
 public class FavoriteAnimeFragment extends BaseBottomSheetDialogFragment {
 
     private static final String TAG = "FavoriteAnimeFragment";
@@ -12,6 +19,13 @@ public class FavoriteAnimeFragment extends BaseBottomSheetDialogFragment {
     @Override
     public String getFragmentName() {
         return TAG;
+    }
+
+    @Override
+    public View onCreateView(final LayoutInflater inflater, final ViewGroup container,
+            final Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.fragment_favorite_anime, container, false);
     }
 
 }
