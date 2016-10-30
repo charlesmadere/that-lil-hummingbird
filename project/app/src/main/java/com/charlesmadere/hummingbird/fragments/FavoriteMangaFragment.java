136948635514881e791ec20dbe52412cefd661be
Charles Madere <charlesrmadere@gmail.com>
2016-10-30 11:53:25 -0700

    filled in some of FavoriteMangaFragment's boilerplate stuff

--- project/app/src/main/java/com/charlesmadere/hummingbird/fragments/FavoriteMangaFragment.java
+++ project/app/src/main/java/com/charlesmadere/hummingbird/fragments/FavoriteMangaFragment.java
@@ -1,6 +1,14 @@
 package com.charlesmadere.hummingbird.fragments;
 
-public class FavoriteMangaFragment extends BaseBottomSheetDialogFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.charlesmadere.hummingbird.R;
+
+public class FavoriteMangaFragment extends BaseBottomSheetDialogFragment implements
+        View.OnClickListener {
 
     private static final String TAG = "FavoriteMangaFragment";
 
@@ -12,6 +20,25 @@ public class FavoriteMangaFragment extends BaseBottomSheetDialogFragment {
     @Override
     public String getFragmentName() {
         return TAG;
+    }
+
+    @Override
+    public void onClick(final View view) {
+        dismissAllowingStateLoss();
+    }
+
+    @Override
+    public View onCreateView(final LayoutInflater inflater, final ViewGroup container,
+            final Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.fragment_favorite_manga, container, false);
+    }
+
+    @Override
+    public void onViewCreated(final View view, final Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // TODO
     }
 
 }
