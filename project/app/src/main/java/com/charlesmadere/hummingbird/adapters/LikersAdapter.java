c28692cc032f23cdf0616d0639e5b101e511fe19
Charles Madere <charlesrmadere@gmail.com>
2016-11-06 11:34:59 -0800

    LikersAdapter now has stable IDs

--- project/app/src/main/java/com/charlesmadere/hummingbird/adapters/LikersAdapter.java
+++ project/app/src/main/java/com/charlesmadere/hummingbird/adapters/LikersAdapter.java
@@ -10,6 +10,16 @@ public class LikersAdapter extends BasePaginationAdapter<Liker> {
 
     public LikersAdapter(final Context context) {
         super(context);
+        setHasStableIds(true);
+    }
+
+    @Override
+    public long getItemId(final int position) {
+        if (isPaginating() && position == getItemCount() - 1) {
+            return Long.MIN_VALUE;
+        } else {
+            return getItem(position).hashCode();
+        }
     }
 
     @LayoutRes
