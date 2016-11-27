b089228208e035d43058f5665872d7da118d1a91
Charles Madere <charlesrmadere@gmail.com>
2016-11-27 15:12:33 -0800

    User.getAvatars() is now a bit more intelligent in what it returns

--- project/app/src/main/java/com/charlesmadere/hummingbird/models/User.java
+++ project/app/src/main/java/com/charlesmadere/hummingbird/models/User.java
@@ -17,6 +17,7 @@ import com.google.gson.JsonParseException;
 import com.google.gson.annotations.SerializedName;
 
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 
 public class User implements Hydratable, Parcelable {
 
@@ -46,9 +47,41 @@ public class User implements Hydratable, Parcelable {
     }
 
     @Nullable
-    public String[] getAvatars() {
-        return new String[] { getAvatarThumb(), getAvatarThumbSmall(), getAvatarSmall(),
-                getAvatarMedium(), getAvatarOriginal() };
+    public ArrayList<String> getAvatars() {
+        final String thumb = getAvatarThumb();
+        final String thumbSmall = getAvatarThumbSmall();
+        final String small = getAvatarSmall();
+        final String medium = getAvatarMedium();
+        final String original = getAvatarOriginal();
+
+        final ArrayList<String> avatars = new ArrayList<>(5);
+
+        if (!TextUtils.isEmpty(thumb)) {
+            avatars.add(thumb);
+        }
+
+        if (!TextUtils.isEmpty(thumbSmall)) {
+            avatars.add(thumbSmall);
+        }
+
+        if (!TextUtils.isEmpty(small)) {
+            avatars.add(small);
+        }
+
+        if (!TextUtils.isEmpty(medium)) {
+            avatars.add(medium);
+        }
+
+        if (!TextUtils.isEmpty(original)) {
+            avatars.add(original);
+        }
+
+        if (avatars.isEmpty()) {
+            return null;
+        } else {
+            avatars.trimToSize();
+            return avatars;
+        }
     }
 
     @Nullable
