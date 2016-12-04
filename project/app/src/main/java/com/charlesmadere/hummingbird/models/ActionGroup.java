3658b84c145fcdb61bb9deade4d4e07c2840eef2
Charles Madere <charlesrmadere@gmail.com>
2016-12-03 19:40:25 -0800

    fixed a json deserialization error

--- project/app/src/main/java/com/charlesmadere/hummingbird/models/ActionGroup.java
+++ project/app/src/main/java/com/charlesmadere/hummingbird/models/ActionGroup.java
@@ -11,7 +11,7 @@ public class ActionGroup implements DataObject, Parcelable {
     @SerializedName("attributes")
     private Attributes mAttributes;
 
-    @SerializedName("dataType")
+    @SerializedName("type")
     private DataType mDataType;
 
     @SerializedName("relationships")
