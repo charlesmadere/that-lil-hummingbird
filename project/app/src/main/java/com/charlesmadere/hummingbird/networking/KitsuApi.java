9cb58277d8f19b944efbee96b207786e150906be
Charles Madere <charlesrmadere@gmail.com>
2016-12-03 19:55:08 -0800

    adjusted KitsuApi getGlobalFeed endpoint to return more data

--- project/app/src/main/java/com/charlesmadere/hummingbird/networking/KitsuApi.java
+++ project/app/src/main/java/com/charlesmadere/hummingbird/networking/KitsuApi.java
@@ -8,7 +8,7 @@ import retrofit2.http.GET;
 
 public interface KitsuApi {
 
-    @GET("feeds/global/global")
+    @GET("feeds/global/global?include=media,actor,unit,subject,subject.user,subject.target_user,subject.media,subject.follower,subject.followed")
     Call<ArrayResponse<ActionGroup>> getGlobalFeed();
 
 }
