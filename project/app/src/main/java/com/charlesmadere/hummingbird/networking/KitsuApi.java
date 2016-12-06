3d0c8a01c3ebe31564a963529a516bb6d06d1579
Charles Madere <charlesrmadere@gmail.com>
2016-12-05 19:53:39 -0800

    added some more KitsuApi calls

--- project/app/src/main/java/com/charlesmadere/hummingbird/networking/KitsuApi.java
+++ project/app/src/main/java/com/charlesmadere/hummingbird/networking/KitsuApi.java
@@ -1,14 +1,23 @@
 package com.charlesmadere.hummingbird.networking;
 
 import com.charlesmadere.hummingbird.models.ActionGroup;
+import com.charlesmadere.hummingbird.models.AnimeV3;
 import com.charlesmadere.hummingbird.models.ArrayResponse;
+import com.charlesmadere.hummingbird.models.UserV3;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 public interface KitsuApi {
 
+    @GET("anime?filter%5Bslug%5D=new-game&include=genres")
+    Call<ArrayResponse<AnimeV3>> getAnime(@Path("anime") String anime);
+
     @GET("feeds/global/global?include=media,actor,unit,subject,subject.user,subject.target_user,subject.media,subject.follower,subject.followed")
     Call<ArrayResponse<ActionGroup>> getGlobalFeed();
+
+    @GET("users?filter%5Bname%5D={username}&include=userRoles.role%2CpinnedPost")
+    Call<ArrayResponse<UserV3>> getUsers(@Path("username") String username);
 
 }
