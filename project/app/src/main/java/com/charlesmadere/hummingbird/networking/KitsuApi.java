482749f6e2470811a2d914f58137111b4cfb097f
Charles Madere <charlesrmadere@gmail.com>
2016-12-05 20:05:07 -0800

    a lil bit more endpoint work

--- project/app/src/main/java/com/charlesmadere/hummingbird/networking/KitsuApi.java
+++ project/app/src/main/java/com/charlesmadere/hummingbird/networking/KitsuApi.java
@@ -3,6 +3,7 @@ package com.charlesmadere.hummingbird.networking;
 import com.charlesmadere.hummingbird.models.ActionGroup;
 import com.charlesmadere.hummingbird.models.AnimeV3;
 import com.charlesmadere.hummingbird.models.ArrayResponse;
+import com.charlesmadere.hummingbird.models.Follow;
 import com.charlesmadere.hummingbird.models.UserV3;
 
 import retrofit2.Call;
@@ -11,8 +12,11 @@ import retrofit2.http.Path;
 
 public interface KitsuApi {
 
-    @GET("anime?filter%5Bslug%5D=new-game&include=genres")
-    Call<ArrayResponse<AnimeV3>> getAnime(@Path("anime") String anime);
+    @GET("anime?filter%5Bslug%5D={slug}&include=genres")
+    Call<ArrayResponse<AnimeV3>> getAnime(@Path("slug") String slug);
+
+    @GET("follows?filter%5Bfollowed%5D={userId}&include=follower&sort=-created_at")
+    Call<ArrayResponse<Follow>> getFollowers(@Path("userId") String userId);
 
     @GET("feeds/global/global?include=media,actor,unit,subject,subject.user,subject.target_user,subject.media,subject.follower,subject.followed")
     Call<ArrayResponse<ActionGroup>> getGlobalFeed();
