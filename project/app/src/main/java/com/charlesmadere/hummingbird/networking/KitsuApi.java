06b88281cd0b0f57cd2ad0166f5fb83e437ab8f1
Charles Madere <charlesrmadere@gmail.com>
2016-12-11 12:28:38 -0800

    fixed endpoint url

--- project/app/src/main/java/com/charlesmadere/hummingbird/networking/KitsuApi.java
+++ project/app/src/main/java/com/charlesmadere/hummingbird/networking/KitsuApi.java
@@ -18,7 +18,7 @@ public interface KitsuApi {
     @GET("follows?filter%5Bfollowed%5D={userId}&include=follower&sort=-created_at")
     Call<ArrayResponse<Follow>> getFollowers(@Path("userId") String userId);
 
-    @GET("feeds/global/global?include=media,actor,unit,subject,subject.user,subject.target_user,subject.media,subject.follower,subject.followed")
+    @GET("feeds/global/global?media,actor,unit,subject,target,target.user.user_roles.role,target.target_user.user_roles.role,target.spoiled_unit,target.media,subject.user.user_roles.role,subject.target_user.user_roles.role,subject.spoiled_unit,subject.media,subject.media,subject.follower,subject.followed,subject.library_entry&page[limit]=10")
     Call<ArrayResponse<ActionGroup>> getGlobalFeed();
 
     @GET("users?filter%5Bname%5D={username}&include=userRoles.role%2CpinnedPost")
